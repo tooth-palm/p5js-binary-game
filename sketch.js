f230446913d725aa7ee46a242e9417d8a611944d
YuichiroHayashizaki <square.lead23@gmail.com>
2023-02-15 19:51:55 +0900

    バイナリのコントローラを保持して描画できるように

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ function setup() {
 
 function draw() {
   drawStage();
+  const binaryManager = new BinaryManager(4, height * 0.9);
+  binaryManager.draw();
 }
 
 function drawStage() {
@@ -31,4 +33,74 @@ function drawStage() {
   noFill();
   line(width * 0.3, height * 0.2, width, height * 0.2);
   pop();
+}
+
+class BinaryManager {
+  #digitNum;
+  #binaryNumbers;
+  #positionY;
+  #selectorIndex;
+
+  constructor(digitNum, positionY) {
+    this.#digitNum = digitNum;
+    this.#binaryNumbers = Array(digitNum).fill(0);
+    this.#positionY = positionY;
+    this.#selectorIndex = 0;
+  }
+
+  draw() {
+    this.#drawNumbers();
+    this.#drawSelector();
+  }
+
+  #drawNumbers() {
+    for (let i = 0; i < this.#digitNum; i++) {
+      const drawPosition = this.#getXFromIndex(i);
+      if (this.#binaryNumbers[i] === 0) {
+        this.#drawZero(drawPosition);
+      } else {
+        this.#drawOne(drawPosition);
+      }
+    }
+  }
+
+  #drawZero(x) {
+    push();
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("0", x, this.#positionY);
+    pop();
+  }
+
+  #drawOne(x) {
+    push();
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("1", x, this.#positionY);
+    pop();
+  }
+
+  #drawSelector() {
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill(255);
+
+    const selectorPosition = this.#getXFromIndex(this.#selectorIndex);
+    const selectorWide = 20;
+    const selectorTall = 5;
+    rect(selectorPosition, this.#positionY + 20, selectorWide, selectorTall);
+    pop();
+  }
+
+  #getXFromIndex(index) {
+    if (this.#digitNum === 1) {
+      return width / 2;
+    }
+
+    const leftX = width * 0.2;
+    const rightX = width * 0.8;
+    const position = map(index, 0, this.#digitNum - 1, leftX, rightX);
+    return position;
+  }
 }
