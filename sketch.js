9844e7e31281d34fdbb06c407b771bc533148231
Hayashizaki <67238088+tooth-palm@users.noreply.github.com>
2023-02-15 21:33:47 +0900

    ユーザが操作する2進数フィールドを作成 (#1)
    * バイナリのコントローラを保持して描画できるように
    
    * 10進数での合計値を表示するように
    
    * バイナリを増減できるように
    
    * キー入力で操作できるように

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
+let binaryManager;
+
 function setup() {
   createCanvas(600, 450);
-  background(0);
-  noLoop();
+  binaryManager = new BinaryManager(4, height * 0.9);
 }
 
 function draw() {
+  background(0);
   drawStage();
+  binaryManager.draw();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    binaryManager.moveSelectorLeft();
+  } else if (keyCode === RIGHT_ARROW) {
+    binaryManager.moveSelectorRight();
+  } else if (keyCode === UP_ARROW) {
+    binaryManager.increment();
+  } else if (keyCode == DOWN_ARROW) {
+    binaryManager.decrement();
+  }
 }
 
 function drawStage() {
@@ -31,4 +46,119 @@ function drawStage() {
   noFill();
   line(width * 0.3, height * 0.2, width, height * 0.2);
   pop();
+}
+
+class BinaryManager {
+  #digitNum;
+  #binaryNumbers;
+  #positionY;
+  #selectorIndex;
+
+  constructor(digitNum, positionY) {
+    this.#digitNum = digitNum;
+    // this.#binaryNumbers = Array(digitNum).fill(0);
+    this.#binaryNumbers = [0, 1, 0, 1];
+    this.#positionY = positionY;
+    this.#selectorIndex = 0;
+  }
+
+  draw() {
+    this.#drawNumbers();
+    this.#drawSelector();
+    this.#drawSumNumber();
+  }
+
+  increment() {
+    if (this.#binaryNumbers[this.#selectorIndex] === 1) return;
+
+    this.#binaryNumbers[this.#selectorIndex]++;
+  }
+
+  decrement() {
+    if (this.#binaryNumbers[this.#selectorIndex] === 0) return;
+
+    this.#binaryNumbers[this.#selectorIndex]--;
+  }
+
+  moveSelectorLeft() {
+    if (this.#selectorIndex === 0) return;
+
+    this.#selectorIndex--;
+  }
+
+  moveSelectorRight() {
+    if (this.#selectorIndex === this.#digitNum - 1) return;
+
+    this.#selectorIndex++;
+  }
+
+  #drawNumbers() {
+    for (let i = 0; i < this.#digitNum; i++) {
+      const drawPosition = this.#getXFromIndex(i);
+      if (this.#binaryNumbers[i] === 0) {
+        this.#drawZero(drawPosition);
+      } else {
+        this.#drawOne(drawPosition);
+      }
+    }
+  }
+
+  #drawZero(x) {
+    push();
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("0", x, this.#positionY);
+    pop();
+  }
+
+  #drawOne(x) {
+    push();
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("1", x, this.#positionY);
+    pop();
+  }
+
+  #drawSelector() {
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill(255);
+
+    const selectorPosition = this.#getXFromIndex(this.#selectorIndex);
+    const selectorWide = 20;
+    const selectorTall = 5;
+    rect(selectorPosition, this.#positionY + 20, selectorWide, selectorTall);
+    pop();
+  }
+
+  #drawSumNumber() {
+    push();
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text(this.#getSumNumber(), width * 0.2, height * 0.7);
+    pop();
+  }
+
+  #getXFromIndex(index) {
+    if (this.#digitNum === 1) {
+      return width / 2;
+    }
+
+    const leftX = width * 0.2;
+    const rightX = width * 0.8;
+    const position = map(index, 0, this.#digitNum - 1, leftX, rightX);
+    return position;
+  }
+
+  #getSumNumber() {
+    let sum = 0;
+    for (let i = 0; i < this.#digitNum; i++) {
+      if (this.#binaryNumbers[i] === 1) {
+        // convert binary to decimal
+        sum += 2 ** (this.#digitNum - i - 1);
+      }
+    }
+    return sum;
+  }
 }
